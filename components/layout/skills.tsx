8a0b46882ae43f05383be1d64ea1d1a74bdf946b
Mohamed Amaan <amaanrc11@gmail.com>
2024-08-01 05:05:40 +0530

    sc

--- components/layout/skills.tsx
+++ components/layout/skills.tsx
@@ -24,7 +24,7 @@ const SkillsSection = () => {
             {category.skills.map((skill, idx) => (
               <motion.li
                 key={idx}
-                className="flex items-center md:py-4 md:px-2 lg:py-4 lg:px-2 py-2 px-2 w-[140px] md:w-[210px] lg:w-[210px] bg-[#1E1E1E] border rounded-lg"
+                className="flex items-center md:py-4 md:px-2 lg:py-4 lg:px-2 py-2 px-2 w-[160px] md:w-[210px] lg:w-[210px] bg-[#1E1E1E] border rounded-lg"
                 variants={fadeInAnimationVariants}
                 initial="initial"
                 whileInView="animate"
