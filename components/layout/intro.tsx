e15de65d07619ac1d38b35e2c270f03505aeb4f5
Mohamed Amaan <amaanrc11@gmail.com>
2024-08-03 11:41:12 +0530

    fix it text issue

--- components/layout/intro.tsx
+++ components/layout/intro.tsx
@@ -188,10 +188,10 @@ export default function Intro() {
             >
               {renderText(words2)}
             </motion.div>
-            <div className="flex flex-col ">
-              <div className="flex flex-row flex-wrap  text-center text-xl sm:text-2xl md:text-3xl lg:text-3xl pt-2 md:pt-3 lg:pt-4 xl:pt-4 font-bold leading-normal tracking-wide text-black dark:text-white">
+            <div className="flex flex-col uppercase">
+              <div className="flex flex-row flex-wrap text-xl sm:text-2xl md:text-3xl lg:text-3xl pt-2 md:pt-3 lg:pt-4 xl:pt-4 font-bold leading-normal tracking-normal text-black dark:text-white">
                 <span className="flex-shrink-0">Software Engineer</span>
-                <span className="flex-shrink-0 ml-1 sm:ml-2">
+                <span className="flex-shrink-0 pl-[0.2px]">
                   <FlipWords words={flipwords} />
                 </span>
               </div>
