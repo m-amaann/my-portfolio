5760b502d5a576deb8fe5c943e749c413d65356a
Mohamed Amaan <amaanrc11@gmail.com>
2024-09-29 22:04:24 +0530

    certificates

--- components/layout/about.tsx
+++ components/layout/about.tsx
@@ -10,8 +10,9 @@ import Link from "next/link";
 export default function About() {
   const { ref } = useSectionInView("About");
 
-  // State to track the active tab
+  // State to track the active tab and certification type
   const [activeTab, setActiveTab] = useState("contact");
+  const [certType, setCertType] = useState("e-learning"); // Default to 'e-learning'
 
   return (
     <motion.section
@@ -26,7 +27,7 @@ export default function About() {
 
       {/* Container for Image and Text */}
       <div className="flex flex-col md:flex-row gap-8 items-start lg:mx-28 md:mx-12 mx-2">
-        
+
         {/* Image Section */}
         <div className="w-full md:w-1/3">
           <div className="w-full aspect-w-1 aspect-h-1 rounded-2xl bg-gradient-to-br from-transparent to-transparent grid place-items-center pt-4 overflow-hidden">
@@ -43,28 +44,20 @@ export default function About() {
 
         {/* Text Section */}
         <div className="text-white md:px-6 lg:px-8 flex flex-col justify-start md:w-2/3">
-          <p className="mt-6 text-gray-600 leading-relaxed pb-4 text-left">
-            As a Software Engineer with 1.5 years of working experience, I am a self-driven individual with a passion for exploring new horizons in the IT field. My expertise lies in crafting dynamic and engaging interfaces through writing clean code and utilizing cutting-edge development tools and techniques. With a strong foundation in both web and mobile development, I excel at creating user-centric applications that deliver seamless experiences.
+          <p className="mt-6 text-gray-600 leading-relaxed pb-4 text-justify">
+            As a Software Engineer with one year of working experience, I am a self-driven individual with a passion for exploring new horizons in the IT field. My expertise lies in crafting dynamic and engaging interfaces through writing clean code and utilizing cutting-edge development tools and techniques. With a strong foundation in both web and mobile development, I excel at creating user-centric applications that deliver seamless experiences.
           </p>
 
           {/* Tab Navigation */}
           <div className="tabs flex justify-start gap-4 mt-6 mb-6">
             <button
-              className={`px-4 py-2 rounded-md ${
-                activeTab === "contact"
-                  ? "bg-blue-500 text-white"
-                  : "bg-gray-200 text-gray-700"
-              }`}
+              className={`relative px-4 py-2 rounded-md ${activeTab === "contact" ? "bg-[#0458AC] text-white" : "bg-gray-200 text-gray-700"}`}
               onClick={() => setActiveTab("contact")}
             >
               Contact Info
             </button>
             <button
-              className={`px-4 py-2 rounded-md ${
-                activeTab === "certifications"
-                  ? "bg-blue-500 text-white"
-                  : "bg-gray-200 text-gray-700"
-              }`}
+              className={`relative px-4 py-2 rounded-md ${activeTab === "certifications" ? "bg-[#0458AC] text-white" : "bg-gray-200 text-gray-700"}`}
               onClick={() => setActiveTab("certifications")}
             >
               Certifications
@@ -82,16 +75,15 @@ export default function About() {
                 transition={{ duration: 0.5 }}
                 className="text-black text-left"
               >
-                <h5 className="text-lg font-semibold mb-4">Contact Information</h5>
-                <ul className="space-y-3 text-gray-600">
+                <ul className="space-y-3 text-[15px] text-gray-600">
                   <li>
-                    <strong>Current Living: </strong>Colombo, Sri Lanka
+                    <span className="font-semibold">Current Living: </span>Colombo, Sri Lanka
                   </li>
                   <li>
-                    <strong>Nationality: </strong>Sri Lankan
+                    <span className="font-semibold">Nationality: </span>Sri Lankan
                   </li>
                   <li>
-                    <strong>Age: </strong>23
+                    <span className="font-semibold">Age: </span>23
                   </li>
                 </ul>
               </motion.div>
@@ -106,45 +98,71 @@ export default function About() {
                 transition={{ duration: 0.5 }}
                 className="text-black text-left"
               >
-                <h5 className="text-lg font-semibold mb-4">Certifications</h5>
-                <ul className="space-y-3 text-gray-600">
-                  <li className="flex items-center gap-3 sm:gap-4">
-                    <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-3xl" />
-                    <span className="flex flex-col">
-                      <strong>BEng (Hons) in Software Engineering: </strong>
-                      <Link href="https://drive.google.com/file/d/1udVbaISge3fOqVxl0Q1uKg4HjHt71vxz/view?usp=sharing" target="_blank" className="text-blue-500 hover:underline">
-                        View Certification <HiExternalLink className="inline ml-1 text-blue-500" />
-                      </Link>
-                    </span>
-                  </li>
-                  <li className="flex items-center gap-3 sm:gap-4">
-                    <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-3xl" />
-                    <span className="flex flex-col">
-                      <strong>Udemy - Spring Boot Complete Certification: </strong>
-                      <Link href="https://www.udemy.com/certificate/UC-ce0e9057-f1b9-4c77-8dd2-643d57455685/" target="_blank" className="text-blue-500 hover:underline">
-                        View Certificate <HiExternalLink className="inline ml-1 text-blue-500" />
-                      </Link>
-                    </span>
-                  </li>
-                  <li className="flex items-center gap-3 sm:gap-4">
-                    <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-3xl" />
-                    <span className="flex flex-col">
-                      <strong>Udemy - Java Absolute Complete Course: </strong>
-                      <Link href="https://www.udemy.com/certificate/UC-448bc9f7-0204-4159-ad23-168483d5bb6a/" target="_blank" className="text-blue-500 hover:underline">
-                        View Certificate <HiExternalLink className="inline ml-1 text-blue-500" />
-                      </Link>
-                    </span>
-                  </li>
-                  <li className="flex items-center gap-3 sm:gap-4">
-                    <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-3xl" />
-                    <span className="flex flex-col">
-                      <strong>Java Programming: </strong>
-                      <Link href="https://www.educba.com/certificate/?c=009KXKRQ0" target="_blank" className="text-blue-500 hover:underline">
-                        View Certificate <HiExternalLink className="inline ml-1 text-blue-500" />
-                      </Link>
-                    </span>
-                  </li>
-                </ul>
+                {/* Certification Type Tabs */}
+                <div className="flex justify-start gap-4 mt-4 mb-6">
+                  <button
+                    className={`relative text-sm text-gray-600 focus:outline-none group ${certType === "professional" ? "font-semibold" : ""}`}
+                    onClick={() => setCertType("professional")}
+                  >
+                    Professional
+                    <span className={`absolute left-0 bottom-0 h-[2px] w-full bg-blue-500 transition-all duration-300 ${certType === "professional" ? "w-full" : "w-0"}`} />
+                  </button>
+                  <button
+                    className={`relative text-sm text-gray-600 focus:outline-none group ${certType === "e-learning" ? "font-semibold" : ""}`}
+                    onClick={() => setCertType("e-learning")}
+                  >
+                    E-Learning
+                    <span className={`absolute left-0 bottom-0 h-[2px] w-full bg-blue-500 transition-all duration-300 ${certType === "e-learning" ? "w-full" : "w-0"}`} />
+                  </button>
+                </div>
+
+                {/* Content based on certification type */}
+                {certType === "professional" && (
+                  <ul className="space-y-3 text-gray-600">
+                    <li className="flex items-center gap-3 sm:gap-4">
+                      <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-2xl" />
+                      <span className="flex flex-col">
+                        <strong className="text-[15px]">BEng (Hons) in Software Engineering: </strong>
+                        <Link href="https://drive.google.com/file/d/1udVbaISge3fOqVxl0Q1uKg4HjHt71vxz/view?usp=sharing" target="_blank" className="text-blue-500 hover:underline text-[14px]">
+                          View Certification <HiExternalLink className="inline ml-1 text-blue-500" />
+                        </Link>
+                      </span>
+                    </li>
+                    {/* Add more professional certificates here */}
+                  </ul>
+                )}
+
+                {certType === "e-learning" && (
+                  <ul className="space-y-3 text-gray-600">
+                    <li className="flex items-center gap-3 sm:gap-4">
+                      <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-2xl" />
+                      <span className="flex flex-col">
+                        <strong className="text-[15px]">Udemy - Spring Boot Complete Certification: </strong>
+                        <Link href="https://www.udemy.com/certificate/UC-ce0e9057-f1b9-4c77-8dd2-643d57455685/" target="_blank" className="text-blue-500 hover:underline text-[14px]">
+                          View Certificate <HiExternalLink className="inline ml-1 text-blue-500" />
+                        </Link>
+                      </span>
+                    </li>
+                    <li className="flex items-center gap-3 sm:gap-4">
+                      <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-2xl" />
+                      <span className="flex flex-col">
+                        <strong className="text-[15px]">Udemy - Java Absolute Complete Course: </strong>
+                        <Link href="https://www.udemy.com/certificate/UC-448bc9f7-0204-4159-ad23-168483d5bb6a/" target="_blank" className="text-blue-500 hover:underline text-[14px]">
+                          View Certificate <HiExternalLink className="inline ml-1 text-blue-500" />
+                        </Link>
+                      </span>
+                    </li>
+                    <li className="flex items-center gap-3 sm:gap-4">
+                      <FaCertificate className="text-blue-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-2xl" />
+                      <span className="flex flex-col">
+                        <strong className="text-[15px]">Java Programming: </strong>
+                        <Link href="https://www.educba.com/certificate/?c=009KXKRQ0" target="_blank" className="text-blue-500 hover:underline text-[14px]">
+                          View Certificate <HiExternalLink className="inline ml-1 text-blue-500" />
+                        </Link>
+                      </span>
+                    </li>
+                  </ul>
+                )}
               </motion.div>
             )}
           </div>
